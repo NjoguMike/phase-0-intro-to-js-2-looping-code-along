ab097c88ff8685c0bc260d2a83b9c9ed4c5a634b
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-07-01 23:35:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+//example -- const people = ['John', 'Otis', 'Kamau']
+
+function writeCards(array1,eventName){
+    const cards = []
+    for(let x = 0; x < array1.length; x++){
+        let messages = `Thank you, ${array1[x]}, for the wonderful ${eventName} gift!`
+        cards.push(messages)
+    }
+    return cards
+}
+
+//console.log(writeCards(people, 'Wedding'))
+
+function countDown(x){
+    while( x >= 0){
+        console.log(x--)
+    }
+    return x
+}
